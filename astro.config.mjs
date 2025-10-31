8f71cfb586b55c59106fcf17e46899ca7e5eab26
Sandra <sandrallamasrizo@gmail.com>
2025-10-31 12:40:16 +0100

    feat: projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import icon from 'astro-icon'; 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()], 
+  integrations: [], 
 });
